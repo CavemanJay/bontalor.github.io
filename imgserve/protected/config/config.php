9a26848b5f1565bb567ea94b2fee02e05ce89025
bontalor <nick.kleespies@gmail.com>
2020-11-02 04:34:16 -0500

    Update config.php

--- imgserve/protected/config/config.php
+++ imgserve/protected/config/config.php
@@ -1,27 +1,27 @@
 <?php
 
-#   _                                                     
-#  (_)    A self-hosted ShareX image serving solution.    
-#   _ _ __ ___   __ _  __ _  ___  ___  ___ _ ____   _____ 
+#   _
+#  (_)    A self-hosted ShareX image serving solution.
+#   _ _ __ ___   __ _  __ _  ___  ___  ___ _ ____   _____
 #  | | '_ ` _ \ / _` |/ _` |/ _ \/ __|/ _ \ '__\ \ / / _ \
 #  | | | | | | | (_| | (_| |  __/\__ \  __/ |   \ V /  __/
 #  |_|_| |_| |_|\__,_|\__, |\___||___/\___|_|    \_/ \___|
-#                      __/ |                              
+#                      __/ |
 #                     |___/   created by github.com/aerouk
 
 /* More information on all these values can be found on the wiki page. */
 /* https://github.com/aerouk/imageserve/wiki/Configuration */
 
-define('RAW_IMAGE', false);
-define('RAW_IMAGE_LINK', false);
-define('IMAGE_EXTENSION', false);
+define('RAW_IMAGE', true);
+define('RAW_IMAGE_LINK', true);
+define('IMAGE_EXTENSION', true);
 
 define('TWITTER_CARDS', true);
 
 // If you're using this, make sure to put a forward slash before.
 // E.g. "/imageserve" not "imageserve"
-define('IMAGESERVE_DIR', "");
-define('TWITTER_HANDLE', "@aerouk_");
+define('IMAGESERVE_DIR', "/imgserve");
+define('TWITTER_HANDLE', "@bontatwitch");
 
-define('APP_NAME', "application name");
-define('PASSKEY', "password goes here");
+define('APP_NAME', "imageserve demo");
+define('PASSKEY', "yo");
