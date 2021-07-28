4faf360fcd0f425e81614e2a1ed3363d576211ce
bontalor <nick.kleespies@gmail.com>
2021-07-28 14:00:33 -0400

    added test stuff

---
+++ yo.js
@@ -0,0 +1,7 @@
+function coolkids(){
+  message = "<img class="raremonkey" src="https://cdn.betterttv.net/emote/55f1cc2b4bbea27f0a7cb210/3x"> I can't wait for mizzy wizzy's stream today ";
+  for(var i=0; i<7; i++){
+    message+=message;
+  }
+  return message
+}
